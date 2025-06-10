a1f3d8acddc06c9c60dd3656b183ffc33d9712dd
jarren <thirdwilliam1@gmail.com>
2025-06-10 16:04:32 +0700

    lesson 5

---
+++ Lesson 5/js/script.js
@@ -0,0 +1,18 @@
+function syncfunc(){
+    console.log("Hello");
+}
+async function asyncfunc(){
+    console.log("Hello world async");
+}
+async function addition(a,b){
+    console.log(a+b);
+    result = a + b
+    document.getElementById("result").innerHTML = result
+    console.log(result);
+}
+function countname(name){
+    return name.length
+}
+function printnamelength(name){
+
+}
